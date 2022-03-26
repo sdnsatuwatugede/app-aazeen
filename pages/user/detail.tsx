794f1d8e93269965e31f2f97fd6fbf6dd840013a
sdnsatuwatugede <sdnwatugede.satu@gmail.com>
2022-03-26 23:34:07 +0700

    main

--- pages/user/detail.tsx
+++ pages/user/detail.tsx
@@ -2,6 +2,7 @@ export default function UserDetail() {
   return (
     <div>
       <p>Halaman User detail</p>
+      <h1>test</h1>
     </div>
   );
 }
---
+++ pages/user/index.tsx
@@ -0,0 +1,7 @@
+export default function User() {
+  return (
+    <div>
+      <p>Halaman User index</p>
+    </div>
+  );
+}
